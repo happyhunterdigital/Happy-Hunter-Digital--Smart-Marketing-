1f4b01c92f860d77d45174871f1b75dd021f1e0e
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2025-12-13 11:48:35 +0200

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,19 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBQvZ2-w9DrJWQEgy4IarClycARAvMJIAc",
+  authDomain: "happyhunterdigital-17480.firebaseapp.com",
+  projectId: "happyhunterdigital-17480",
+  storageBucket: "happyhunterdigital-17480.firebasestorage.app",
+  messagingSenderId: "449102421348",
+  appId: "1:449102421348:web:d61e0c209b93bf282fae71",
+  measurementId: "G-PS04HKGEXF"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app);
+export { db };
