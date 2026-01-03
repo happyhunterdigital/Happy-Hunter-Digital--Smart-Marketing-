2b53e89e28372a14304c83dac4090ca6b6968a13
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-01-03 11:24:45 +0200

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,18 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore"; // Required for the database
+import { getAnalytics } from "firebase/analytics"; // Optional, but good to have
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQvZ2-w9DrJWQEgy4IarClycARAvMJIAc",
-  authDomain: "happyhunterdigital-17480.firebaseapp.com",
-  projectId: "happyhunterdigital-17480",
-  storageBucket: "happyhunterdigital-17480.firebasestorage.app",
-  messagingSenderId: "449102421348",
-  appId: "1:449102421348:web:d61e0c209b93bf282fae71",
-  measurementId: "G-PS04HKGEXF"
+  apiKey: "AIzaSyAZHcYL0_NP0teaUOT60YeZbfQUzk8KfEk",
+  authDomain: "happy-hunter-systems.firebaseapp.com",
+  projectId: "happy-hunter-systems",
+  storageBucket: "happy-hunter-systems.firebasestorage.app",
+  messagingSenderId: "629161289232",
+  appId: "1:629161289232:web:17ff042c3f96ecbeb78c95",
+  measurementId: "G-4GDXX24PP0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+// Initialize Services
+export const db = getFirestore(app); // <--- CRITICAL: This allows us to save leads
+const analytics = getAnalytics(app);
