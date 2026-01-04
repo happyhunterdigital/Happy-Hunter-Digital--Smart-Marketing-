28f1a32b888aa355145a8c97503f16c646c6ed58
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-01-04 20:11:09 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-// IMPORT THE NEW FILE WE JUST CREATED
-import { Navbar } from './components/Navbar'; 
+
+// --- COMPONENTS ---
+import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { AiAudit } from './components/AiAudit';
 import { Services } from './components/Services';
 import { Portfolio } from './components/Portfolio';
 import { Footer } from './components/Footer';
 import { WhatsAppWidget } from './components/WhatsAppWidget';
-import { EarnedMedia } from './components/EarnedMedia';
 import { BlogReader } from './components/BlogReader';
 import { PrivacyPolicy } from './components/PrivacyPolicy';
 import { CookieConsent } from './components/CookieConsent';
-import AdminDashboard from './pages/AdminDashboard'; 
 
+// --- PAGES ---
+import { EarnedMedia } from './components/EarnedMedia'; // Note: Check if this is in components or pages folder in your structure
+import { CoreServices } from './pages/CoreServices';
+import AdminDashboard from './pages/AdminDashboard';
+
+// --- LAYOUT WRAPPER (Hides Nav/Footer on Admin) ---
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
   const isAdmin = location.pathname === '/admin';
 
   return (
     <div className="min-h-screen flex flex-col font-sans selection:bg-brand-yellow selection:text-brand-dark">
-      {/* USE THE NEW NAVBAR HERE */}
-      {!isAdmin && <Navbar />} 
+      {!isAdmin && <Navbar />}
       
       <main className="flex-grow">
         {children}
@@ -34,6 +38,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
+// --- HOME PAGE COMPONENT ---
 const HomePage = () => (
   <div className="animate-fade-in">
     <Hero />
@@ -43,15 +48,20 @@ const HomePage = () => (
   </div>
 );
 
+// --- MAIN APP COMPONENT ---
 const App: React.FC = () => {
   return (
     <Router>
       <Layout>
         <Routes>
+          {/* Main Website Routes */}
           <Route path="/" element={<HomePage />} />
+          <Route path="/core-services" element={<CoreServices />} />
           <Route path="/earned-media" element={<EarnedMedia />} />
           <Route path="/blog/:id" element={<BlogReader />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          
+          {/* Admin Route */}
           <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </Layout>
