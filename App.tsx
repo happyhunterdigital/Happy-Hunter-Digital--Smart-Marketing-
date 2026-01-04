22a02bc6d49a34d44d527baf43239cdbc98b2c16
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-01-04 18:25:37 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-// 1. CHANGED: Import the new Navbar instead of Header
+// IMPORT THE NEW FILE WE JUST CREATED
 import { Navbar } from './components/Navbar'; 
 import { Hero } from './components/Hero';
 import { AiAudit } from './components/AiAudit';
@@ -14,14 +14,13 @@ import { PrivacyPolicy } from './components/PrivacyPolicy';
 import { CookieConsent } from './components/CookieConsent';
 import AdminDashboard from './pages/AdminDashboard'; 
 
-// Layout wrapper to hide Navbar/Footer on Admin page
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
   const isAdmin = location.pathname === '/admin';
 
   return (
     <div className="min-h-screen flex flex-col font-sans selection:bg-brand-yellow selection:text-brand-dark">
-      {/* 2. CHANGED: Use the new Navbar component here */}
+      {/* USE THE NEW NAVBAR HERE */}
       {!isAdmin && <Navbar />} 
       
       <main className="flex-grow">
@@ -35,7 +34,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-// The Home Page (All sections in one scrollable page)
 const HomePage = () => (
   <div className="animate-fade-in">
     <Hero />
@@ -50,13 +48,10 @@ const App: React.FC = () => {
     <Router>
       <Layout>
         <Routes>
-          {/* Main Website Routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/earned-media" element={<EarnedMedia />} />
           <Route path="/blog/:id" element={<BlogReader />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          
-          {/* The Hidden Admin Route */}
           <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </Layout>
