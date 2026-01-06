5e7b1823e6fbe40527e62a86a9f13571069ec064
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-01-06 14:12:14 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-
-// --- COMPONENTS ---
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { AiAudit } from './components/AiAudit';
 import { Services } from './components/Services';
 import { Portfolio } from './components/Portfolio';
 import { Footer } from './components/Footer';
-
-// --- BOTS & TOOLS ---
 import { WhatsAppWidget } from './components/WhatsAppWidget';
 import { Chatbot } from './components/Chatbot'; 
-// NOTE: We removed the ApiTest import to clean up the site
-
-// --- PAGES ---
 import { EarnedMedia } from './components/EarnedMedia'; 
 import { BlogReader } from './components/BlogReader';
 import { PrivacyPolicy } from './components/PrivacyPolicy';
@@ -26,15 +19,11 @@ import AdminDashboard from './pages/AdminDashboard';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
   const isAdmin = location.pathname === '/admin';
-
   return (
     <div className="min-h-screen flex flex-col font-sans selection:bg-brand-yellow selection:text-brand-dark">
-      {/* API Test Panel Removed - Site is now Live */}
-
       {!isAdmin && <Navbar />}
       <main className="flex-grow">{children}</main>
       {!isAdmin && <Footer />}
-      
       <WhatsAppWidget />
       <Chatbot />
       <CookieConsent />
