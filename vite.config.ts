d4695abe815a349218fd3cf78f54eebfc8d1c970
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-01-25 20:42:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Stability: Ensures mixed modules (commonjs/esm) don't crash the build
     commonjsOptions: {
       transformMixedEsModules: true,
     },
