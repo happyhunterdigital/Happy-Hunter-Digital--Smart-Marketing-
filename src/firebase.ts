73461a0c73c2ed50cd8512d677051b9997361b9e
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-02-02 20:50:12 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,6 +2,5 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG || "{}");
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
