dc09f4ff84f227f4070886d8172e16dad58a865e
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-02-16 16:09:42 +0200

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+// 🛡️ IRON DOME: Centralized component exports
+// Prevents case sensitivity issues on Linux builds
+
+export { default as Navbar } from './Navbar';
+export { default as Chatbot } from './Chatbot';
+export { default as Footer } from './Footer';
+export { default as CookieConsent } from './CookieConsent';
+export { default as EventPopup } from './EventPopup';
+export { default as ContentRibbon } from './ContentRibbon';
+export { default as AiAudit } from './AiAudit';
+export { default as GlobalErrorBoundary } from './GlobalErrorBoundary';
