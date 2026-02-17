52df374d28f634bba6f883deb0d7bca9aa5d18c4
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-02-17 16:13:20 +0200

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,9 @@
-// 🛡️ IRON DOME: Centralized component exports
-// Prevents case sensitivity issues on Linux builds
-
-export { default as Navbar } from './Navbar';
+// src/components/index.ts
+export { default as AiAudit } from './AiAudit';
 export { default as Chatbot } from './Chatbot';
-export { default as Footer } from './Footer';
+export { default as ContentRibbon } from './ContentRibbon';
 export { default as CookieConsent } from './CookieConsent';
 export { default as EventPopup } from './EventPopup';
-export { default as ContentRibbon } from './ContentRibbon';
-export { default as AiAudit } from './AiAudit';
-export { default as GlobalErrorBoundary } from './GlobalErrorBoundary';
+export { default as Footer } from './Footer';
+export { default as GlobalErrorBoundary } from './GlobalErrorBoundary'; // ADD THIS
+export { default as Navbar } from './Navbar';
