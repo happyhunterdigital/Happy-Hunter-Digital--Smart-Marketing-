46018bf9be839561937fec66f1fc411ee7f32162
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-02-18 08:42:49 +0200

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
-// src/components/index.ts
 export { default as AiAudit } from './AiAudit';
 export { default as Chatbot } from './Chatbot';
 export { default as ContentRibbon } from './ContentRibbon';
 export { default as CookieConsent } from './CookieConsent';
 export { default as EventPopup } from './EventPopup';
 export { default as Footer } from './Footer';
-export { default as GlobalErrorBoundary } from './GlobalErrorBoundary'; // ADD THIS
+export { default as GlobalErrorBoundary } from './GlobalErrorBoundary';
 export { default as Navbar } from './Navbar';
